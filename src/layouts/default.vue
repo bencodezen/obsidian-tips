ebfc9ffcf11edd352839fa022f360f17d0016e9e
Ben Hong <ben@bencodezen.io>
2021-12-31 21:38:12 -0500

    feature: remove placeholder logo

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,5 @@
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
     <div class="wrapper">
       <HelloWorld :msg="$frontmatter.title" />
     </div>
