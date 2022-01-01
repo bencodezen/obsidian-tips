c7974d53ecb983e2bc35f3f41fc8904182d925e3
Ben Hong <ben@bencodezen.io>
2021-12-31 21:36:08 -0500

    feature: update landing page content

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,31 +1,45 @@
 <template>
   <WelcomeItem>
     <template #icon><DocumentationIcon /></template>
-    <template #heading>Documentation</template>
+    <template #heading>Blog</template>
 
-    îles’s
-    <a target="_blank" href="https://iles-docs.netlify.app/">documentation</a>
-    is in progress, but it should provide enough info to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon><ToolingIcon /></template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite</a>. The recommended IDE
-    setup is <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>.
+    <p>
+      Check back for short and concise articles where I share some of the most
+      useful techniques and tools I've found that have helped me use Obsidian.
+    </p>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon><CommunityIcon /></template>
-    <template #heading>Community</template>
+    <template #heading>Newsletter</template>
 
-    Got stuck? Ask your question on the
-    <a target="_blank" href="https://discord.gg/xhheEDwUMX">#iles channel</a> of the Vite.js Discord
-    server. Follow the
-    <a target="_blank" href="https://twitter.com/ilesjs">@ilesjs</a>
-    twitter account for latest news.
+    <p>
+      If you want the tips sent to your inbox, sign up for a once a month email
+      that will go out at the end of each month. Your email will never be used
+      for spam or shared with any third parties.
+    </p>
+
+    <form name="contact" method="POST" data-netlify="true">
+      <div class="base-input-wrapper">
+        <label for="name">Name: </label>
+        <input type="text" name="name" style="padding: 5px" />
+      </div>
+      <div class="base-input-wrapper">
+        <label for="email">Email: </label>
+        <input type="text" name="email" style="padding: 5px" />
+      </div>
+      <p>
+        <button type="submit" style="padding: 0.5rem">Submit</button>
+      </p>
+    </form>
   </WelcomeItem>
 </template>
+
+<style scoped>
+.base-input-wrapper {
+  display: grid;
+  grid-template-columns: 50px 1fr;
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+</style>
